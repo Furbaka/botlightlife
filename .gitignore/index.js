a64f5d72ee667ea129217d56a8cd058d102a16c1
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-03-19 22:54:26 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -1,36 +1,28 @@
-const Discord = require("discord.js");
-var bot = new Discord.Client();
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+var prefix = ("*")
+
+bot.on('ready', function() {
+    bot.user.setGame("Command: !help");
+    console.log("Connected");
+});
+
 bot.login(process.env.TOKEN);
-var prefix = (":::");
 
-bot.on("message", message => {
 
-	if(message.content === prefix + "info") {
-		var embed = new Discord.RichEmbed()
-		.setDescription("Informations du serveur")
-		.addField("Nom du serveur", message.guild.name)
-		.addfield("Créé le", message.guild.createdAt)
-		.addField("Utilisateurs sur le serveurs", message.guild.membercount)
-		.setColor("0x0000FF")
-	message.channel.sendEmbed(embed)
+bot.on('message', message => {
+    if (message.content === prefix + "help"){
+        message.channel.sendMessage("Liste des commandes: \n !burgerbot \n !fabriquant");
+    }
 
-	}
+    if (message.content === prefix + "burgerbot"){
+        message.channe("Création du bot le _11/12/2017_ à _13h57_");
+        console.log("Commande effectué");
+    }
 
-	if (message.content.startWith(prefix + "sondage")) {
-		if(message.author.id == "328514801124900866"){
-			let args = message.content.split(" ").slice(1);
-			let thingToEcho = args.join(" ")
-			var embed = new Discord.RichEmbed()
-				.setDescription("Sondage")
-				.addField(thingToEcho, "Répondre avec :white_check_mark: ou :x:")
-				.setColor("0xB40404")
-				.setTimestamp()
-			message.guild.channels.find("name", "sondage").sendEmbed(embed)
-			.then(function (message) {
-			message.react("✔")
-			message.react("❌")
-		}).catch(function() {
-		});
-		}else{
-			return message.reply("Tu n'as pas la permission.")
-}}})
+    if (message.content === prefix + "fabriquant"){
+        message.reply("_Ce bot a été crée par MhBurgerKing_");
+        console.log("Commande effectué");
+    }
+});
