0f3c7f5df786175907285a3576dff1bd4dd6eb13
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-03-20 09:42:21 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -21,6 +21,20 @@ bot.on('message', message => {
         console.log("Commande effectué");
     }
 
+    if (message.content === prefix + "support"){
+        var embed = new Discord.RichEmbed()
+            .setTitle("SUPPORT")
+            .setDescription("Page de support")
+            .addField("Problème", "Envoie **1a** à MhBurgerKing en message privée", true)
+            .addField("Question", "Envoie **2b** en message privée", true)
+            .addField("Autre", "Envoie **3c** à MhBurgerKing en message privée", true)
+            .setColor("0xF4D03F")
+            .addField("Apprend à coder un bot discord !", "Suivez les tuto de [PZH Codage](https://www.youtube.com/c/pzhcodage) sur sa chaîne youtube !", true)
+            .setFooter("La réponse de MhBurgerKing n'est pas immédiate, faites preuve de patience. :) Bon moment parmis la PZH's Community")
+        message.channel.sendEmbed(embed);
+    
+
+    }
     if (message.content === prefix + "troprigolo"){
         message.channel.sendMessage("JE T'AIME PAS !");
         console.log("Commande effectué");
