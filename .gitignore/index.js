26ae0bc21bb56023bfb2fa310525f3de2b2fff02
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-03-20 15:39:45 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("*")
 
 bot.on('ready', function() {
-    bot.user.setGame("message.guild.memberCount");
+    bot.user.setGame(guild.memberCount);
     console.log("Connected");
 });
 
