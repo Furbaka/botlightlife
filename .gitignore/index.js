7f21a1ef5c672230fc1bb5190337002d2069880b
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-03-20 16:08:36 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -4,7 +4,7 @@ const bot = new Discord.Client();
 var prefix = ("*")
 
 bot.on('ready', function() {
-    bot.user.setGame(MANGER);
+    bot.user.setGame("MANGER");
     console.log("Connected");
 });
 
