d217478083835abf5f7f7aaf1e72f2eebd6dd1a0
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-03-21 12:42:52 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -52,22 +52,25 @@ bot.on('message', message => {
         console.log("Commande effectué");
     }
     
-    if (message.content == "DO YOU KNOW DA WAE"){
-        message.channel.sendMessage("Follow me ma brudda, I know da wae !");
+    if (message.content == "DO YOU KNOW DA WAE ?" || message.content == "do you know da wae ?" || message.content == "DO YOU KNOW DA WAE" || message.content == "do you know da wae" ){
+        message.channel.sendMessage("Follow me ma brudda, I know da wae ! /n https://www.google.com/maps/place/Uganda/@1.3671051,30.058145,7z/data=!3m1!4b1!4m5!3m4!1s0x1771a69f6499f945:0x874155ce43014549!8m2!3d1.373333!4d32.290275");
     }
 
     if (message.content == "PÂTES" || message.content == "BURGER" || message.content == "PIZZA"){
         message.channel.sendMessage("Arrêtez de parler de bouffe j'ai faim :/");
     }
     
-    if (message.content == "DAB"){
+    if (message.content == "DAB" || message.content == "dab"){
         message.channel.sendMessage("LE DAB NÉ PAS MORT");
     }
 
-    if (message.content == "BOT"){
+    if (message.content == "BOT" || message.content == "bot"){
         message.channel.sendMessage("Donc... je suis pas un vrai petit garcon ? :/");
     }
     
+    if (message.content == "Quelle heure il est ?") {
+        message.channel.sendMessage("L'HEURE DE T'ACHETER UNE MONTRE !");
+    }
     if (message.content == "Quelle heure il est ?") {
         message.channel.sendMessage("L'HEURE DE T'ACHETER UNE MONTRE !");
     }
