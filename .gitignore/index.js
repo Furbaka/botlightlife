740b273b9b22dbfa5914b9f57afd800333da6d59
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-03-23 11:46:24 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -13,20 +13,6 @@ bot.login(process.env.TOKEN);
    
 bot.on('message', message => {
     
-    if (!args) return message.reply("You must have something to vote for!")
-    if (!message.content.includes("?")) return message.reply("Include a ? in your vote!")
-        message.channel.send(`:ballot_box:  ${message.author.username} started a vote! React to my next message to vote on it. :ballot_box: `);
-        const pollTopic = await message.channel.send(message.content.slice(2));
-        await pollTopic.react(`✅`);
-        await pollTopic.react(`⛔`);
-        // Create a reaction collector
-        const filter = (reaction) => reaction.emoji.name === '✅';
-        const collector = pollTopic.createReactionCollector(filter, { time: 15000 });
-        collector.on('collect', r => console.log(`Collected ${r.emoji.name}`));
-        collector.on('end', collected => console.log(`Collected ${collected.size} items`));
-    
-    }
-       
     if (message.content === prefix + "aide"){
         var embed2 = new Discord.RichEmbed()
             .setTitle("AIDE")
