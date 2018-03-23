5c6998e7d8424e17936782af00e7649a2695d474
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-03-23 11:49:59 +0100

    Update index.js

--- .gitignore/index.js
+++ .gitignore/index.js
@@ -53,7 +53,7 @@ bot.on('message', message => {
     }
     
     if (message.content == "DO YOU KNOW DA WAE ?" || message.content == "do you know da wae ?" || message.content == "DO YOU KNOW DA WAE" || message.content == "do you know da wae" ){
-        message.channel.sendMessage("Follow me ma brudda, I know da wae ! /n https://www.google.com/maps/place/Uganda/@1.3671051,30.058145,7z/data=!3m1!4b1!4m5!3m4!1s0x1771a69f6499f945:0x874155ce43014549!8m2!3d1.373333!4d32.290275");
+        message.channel.sendMessage("Follow me ma brudda, I know da wae ! https://www.google.com/maps/place/Uganda/@1.3671051,30.058145,7z/data=!3m1!4b1!4m5!3m4!1s0x1771a69f6499f945:0x874155ce43014549!8m2!3d1.373333!4d32.290275");
     }
 
     if (message.content == "pates" || message.content == "burger" || message.content == "pizza" ||  message.content == "pâtes"){
