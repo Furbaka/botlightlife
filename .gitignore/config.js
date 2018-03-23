809ac63235a2251c92f4a0bcb61e43cd2c62beee
Mistigrix <37482633+Mistigrix@users.noreply.github.com>
2018-03-23 11:58:25 +0100

    Create config.js

---
+++ .gitignore/config.js
@@ -0,0 +1,10 @@
+{
+	"self_token": "",
+	"user_id": "",
+	"bot_name": "L↓GHT L↑FE",
+	"owner_name": "",
+	"prefix": "*",
+	"msg_timeout": 3000,
+	"error_color": 12731971,
+	"success_color": 37765
+}
